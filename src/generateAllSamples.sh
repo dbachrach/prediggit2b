73ae05b4254eb227b28c5b60456d2b89b599ab14
canunu <canunu@66d00306-0140-082f-e7ee-ac84a8be3922>
2010-04-11 04:04:29 +0000

    added shell scripts to automate feature, training (sample) and test set generation

---
+++ src/generateAllSamples.sh
@@ -0,0 +1,66 @@
+#/bin/bash
+
+python3 generateRandomSample.py ../Jan1-15/features.db 250 ../Jan1-15/sample1.db
+python3 generateRandomSample.py ../Jan16-31/features.db 250 ../Jan16-31/sample1.db
+python3 generateRandomSample.py ../Feb1-15/features.db 250 ../Feb1-15/sample1.db
+python3 generateRandomSample.py ../Feb16-28/features.db 250 ../Feb16-28/sample1.db
+python3 generateRandomSample.py ../Mar1-15/features.db 250 ../Mar1-15/sample1.db
+python3 generateRandomSample.py ../Mar16-31/features.db 250 ../Mar16-31/sample1.db
+python3 generateRandomSample.py ../Apr1-15/features.db 250 ../Apr1-15/sample1.db
+python3 generateRandomSample.py ../Jan/features.db 1000 ../Jan/sample1.db
+python3 generateRandomSample.py ../Feb/features.db 1000 ../Feb/sample1.db
+python3 generateRandomSample.py ../Mar/features.db 1000 ../Mar/sample1.db
+python3 generateRandomSample.py ../Jan-Mar/features.db 5000 ../Jan-Mar/sample1.db
+python3 generateRandomSample.py ../Feb-Apr/features.db 5000 ../Feb-Apr/sample1.db
+
+python3 generateRandomSample.py ../Jan1-15/features.db 250 ../Jan1-15/sample2.db
+python3 generateRandomSample.py ../Jan16-31/features.db 250 ../Jan16-31/sample2.db
+python3 generateRandomSample.py ../Feb1-15/features.db 250 ../Feb1-15/sample2.db
+python3 generateRandomSample.py ../Feb16-28/features.db 250 ../Feb16-28/sample2.db
+python3 generateRandomSample.py ../Mar1-15/features.db 250 ../Mar1-15/sample2.db
+python3 generateRandomSample.py ../Mar16-31/features.db 250 ../Mar16-31/sample2.db
+python3 generateRandomSample.py ../Apr1-15/features.db 250 ../Apr1-15/sample2.db
+python3 generateRandomSample.py ../Jan/features.db 1000 ../Jan/sample2.db
+python3 generateRandomSample.py ../Feb/features.db 1000 ../Feb/sample2.db
+python3 generateRandomSample.py ../Mar/features.db 1000 ../Mar/sample2.db
+python3 generateRandomSample.py ../Jan-Mar/features.db 5000 ../Jan-Mar/sample2.db
+python3 generateRandomSample.py ../Feb-Apr/features.db 5000 ../Feb-Apr/sample2.db
+
+python3 generateRandomSample.py ../Jan1-15/features.db 250 ../Jan1-15/sample3.db
+python3 generateRandomSample.py ../Jan16-31/features.db 250 ../Jan16-31/sample3.db
+python3 generateRandomSample.py ../Feb1-15/features.db 250 ../Feb1-15/sample3.db
+python3 generateRandomSample.py ../Feb16-28/features.db 250 ../Feb16-28/sample3.db
+python3 generateRandomSample.py ../Mar1-15/features.db 250 ../Mar1-15/sample3.db
+python3 generateRandomSample.py ../Mar16-31/features.db 250 ../Mar16-31/sample3.db
+python3 generateRandomSample.py ../Apr1-15/features.db 250 ../Apr1-15/sample3.db
+python3 generateRandomSample.py ../Jan/features.db 1000 ../Jan/sample3.db
+python3 generateRandomSample.py ../Feb/features.db 1000 ../Feb/sample3.db
+python3 generateRandomSample.py ../Mar/features.db 1000 ../Mar/sample3.db
+python3 generateRandomSample.py ../Jan-Mar/features.db 5000 ../Jan-Mar/sample3.db
+python3 generateRandomSample.py ../Feb-Apr/features.db 5000 ../Feb-Apr/sample3.db
+
+python3 generateRandomSample.py ../Jan1-15/features.db 250 ../Jan1-15/sample4.db
+python3 generateRandomSample.py ../Jan16-31/features.db 250 ../Jan16-31/sample4.db
+python3 generateRandomSample.py ../Feb1-15/features.db 250 ../Feb1-15/sample4.db
+python3 generateRandomSample.py ../Feb16-28/features.db 250 ../Feb16-28/sample4.db
+python3 generateRandomSample.py ../Mar1-15/features.db 250 ../Mar1-15/sample4.db
+python3 generateRandomSample.py ../Mar16-31/features.db 250 ../Mar16-31/sample4.db
+python3 generateRandomSample.py ../Apr1-15/features.db 250 ../Apr1-15/sample4.db
+python3 generateRandomSample.py ../Jan/features.db 1000 ../Jan/sample4.db
+python3 generateRandomSample.py ../Feb/features.db 1000 ../Feb/sample4.db
+python3 generateRandomSample.py ../Mar/features.db 1000 ../Mar/sample4.db
+python3 generateRandomSample.py ../Jan-Mar/features.db 5000 ../Jan-Mar/sample4.db
+python3 generateRandomSample.py ../Feb-Apr/features.db 5000 ../Feb-Apr/sample4.db
+
+python3 generateRandomSample.py ../Jan1-15/features.db 250 ../Jan1-15/sample5.db
+python3 generateRandomSample.py ../Jan16-31/features.db 250 ../Jan16-31/sample5.db
+python3 generateRandomSample.py ../Feb1-15/features.db 250 ../Feb1-15/sample5.db
+python3 generateRandomSample.py ../Feb16-28/features.db 250 ../Feb16-28/sample5.db
+python3 generateRandomSample.py ../Mar1-15/features.db 250 ../Mar1-15/sample5.db
+python3 generateRandomSample.py ../Mar16-31/features.db 250 ../Mar16-31/sample5.db
+python3 generateRandomSample.py ../Apr1-15/features.db 250 ../Apr1-15/sample5.db
+python3 generateRandomSample.py ../Jan/features.db 1000 ../Jan/sample5.db
+python3 generateRandomSample.py ../Feb/features.db 1000 ../Feb/sample5.db
+python3 generateRandomSample.py ../Mar/features.db 1000 ../Mar/sample5.db
+python3 generateRandomSample.py ../Jan-Mar/features.db 5000 ../Jan-Mar/sample5.db
+python3 generateRandomSample.py ../Feb-Apr/features.db 5000 ../Feb-Apr/sample5.db
